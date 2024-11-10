e46976a1d819bacb6b156f7f1cc6cb1f1a372f8a
CamrynW <camryn.wutke@alumni.ubc.ca>
2024-11-10 15:49:55 -0800

    profile

---
+++ Profile/profile.js
@@ -0,0 +1,42 @@
+const entriesList = document.getElementById('previousEntries');
+
+const getJournalEntries = () => {
+    let journal;
+    if(localStorage.getItem('journal') === null){
+        journal = [];
+    }else {
+        journal = JSON.parse(localStorage.getItem('journal'));
+    }
+    return journal;
+}
+
+const saveJournalEntry = entry => {
+    const journal = getJournalEntries();
+    journal.push(entry);
+    localStorage.setItem('journal', JSON.stringify(journal));
+}
+
+const addJournalEntry = e => {
+    e.preventDefault();
+    const input = document.getElementById('newJournalEntry');
+
+    saveJournalEntry(input.value);
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    entriesList.appendChild(li);
+
+    input.value = '';
+}
+
+const displayJournalEntries = () => {
+    const journalEntries = getJournalEntries();
+    entriesList.innerHTML = '';
+
+    journalEntries.reverse().forEach((entry) => {
+        const li = document.createElement('li');
+        li.textContent = entry;
+        entriesList.appendChild(li);
+    });
+};
+
+window.onload = displayJournalEntries;
